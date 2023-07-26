5e2c1c03be9281251274e063d31b861f13f9535d
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2023-07-27 00:10:31 +0700

    feat: :sparkles: finish

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,41 @@
 /// <reference types="jquery" />
 
-console.log("Hello World!");
+// window onload
+$(window).on("load", function () {
+  var options = {
+    animationEnabled: true,
+    title: {
+      text: "",
+    },
+    axisX: {
+      valueFormatString: "MMM",
+    },
+    axisY: {
+    //   title: "Sales (in USD)",
+      prefix: "$",
+    },
+    data: [
+      {
+        yValueFormatString: "$#,###",
+        xValueFormatString: "MMMM",
+        type: "spline",
+        dataPoints: [
+          { x: new Date(2017, 0), y: 25060 },
+          { x: new Date(2017, 1), y: 27980 },
+          { x: new Date(2017, 2), y: 33800 },
+          { x: new Date(2017, 3), y: 49400 },
+          { x: new Date(2017, 4), y: 40260 },
+          { x: new Date(2017, 5), y: 33900 },
+          { x: new Date(2017, 6), y: 48000 },
+          { x: new Date(2017, 7), y: 31500 },
+          { x: new Date(2017, 8), y: 32300 },
+          { x: new Date(2017, 9), y: 42000 },
+          { x: new Date(2017, 10), y: 52160 },
+          { x: new Date(2017, 11), y: 49400 },
+        ],
+      },
+    ],
+  };
+
+  ($("#chartContainer") as any).CanvasJSChart(options);
+});
